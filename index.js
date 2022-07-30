5a2aa64cc0a64099c0dfb1ca68d4b23d82e9f6fa
Laitungcute <laitung27122002@gmail.com>
2022-07-31 00:08:28 +0700

    Demo javascript

---
+++ index.js
@@ -0,0 +1,54 @@
+// console.log("hello world");
+// alert("Hello world");
+// myName = 'Hoàng Tùng';
+// console.log(myName);
+
+// var myName = "Tùng";
+// console.log(myName);
+// let, const
+// i=5;
+// console.log(i);
+
+// for(var i = 0;i<4;i++) {
+//     console.log(i);
+// }
+
+// console.log(i);
+
+// console.log(myName1);
+
+// let myName1 = "Tùng";
+// console.log(myName1);
+// myName1 = "Lại Tùng";
+// console.log(myName1);
+
+// for(let i = 0;i<4;i++) {
+//     console.log(i);
+// }
+
+// const myName2 = "Hoàng Tùng";
+// console.log(myName2);
+// myName2 = "Lại Tùng";
+// console.log(myName2);
+
+// console.log(i);
+
+// let myName = 'Tùng';
+// console.log(myName);
+// let tuoi = 20;
+// console.log(tuoi);
+
+// a = '1';
+// b = 2;
+// c = a+b;
+// console.log(c);
+// console.log(typeof(c));
+
+let a = '11';
+let b = 1;
+let c = a*1+b;  
+console.log(c);
+console.log(typeof(c));
+
+// alert(), consle.log(), var, let, const, cách phân biệt các kiểu dữ liệu,
+// Vd demo các cái ảo ma lazada
